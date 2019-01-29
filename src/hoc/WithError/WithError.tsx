e474509e17404ac9711ef4ad8eaea8be00d46e13
Nutod <alukodotun@gmail.com>
2019-01-29 21:40:55 +0100

    Added ErrorBoundary Logic

---
+++ src/hoc/WithError/WithError.tsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class ErrorBoundary extends Component {
+	state = {
+		error: null
+	};
+
+	componentDidCatch = (error: any) => {
+		this.setState({ error });
+	};
+
+	render() {
+		const { children } = this.props;
+
+		if (this.state.error) {
+			return <h2>Something went wrong!!!</h2>;
+		}
+
+		return <>{children}</>;
+	}
+}
