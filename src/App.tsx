479d025d0a0a07aa0d43c288321a37a370fa4af9
Nutod <alukodotun@gmail.com>
2019-01-31 10:45:44 +0100

    Added new todo

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Layout from "./container/Layout";
 import WithError from "./hoc/WithError/WithError";
 
 // TODO: Add PropType validation
+// TODO: Add Component tests
 export default function App() {
 	return (
 		<WithError>
