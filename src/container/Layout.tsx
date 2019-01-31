27392c522693e94ce131d7f48e743680b2f58e9c
Nutod <alukodotun@gmail.com>
2019-01-31 10:33:34 +0100

    Added Footer to Layout Component

--- src/container/Layout.tsx
+++ src/container/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Footer from "../components/Footer/Footer";
 
 export default function Layout() {
 	return (
 		<>
 			<Hero />
+			<Footer />
 		</>
 	);
 }
