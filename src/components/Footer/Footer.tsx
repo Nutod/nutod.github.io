a61ca2c3d07c16a56fde7cb577183cac586bc26b
Nutod <alukodotun@gmail.com>
2019-01-31 10:33:11 +0100

    Added Logic for Footer Component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FooterWrapper } from "../styles/Wrapper";
+import { MailText, NumberText, CopyrightText } from "../styles/Texts";
+
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+
+export default function Footer() {
+	return (
+		<FooterWrapper>
+			<div>
+				You can say hi to me here{" "}
+				<span role="img" aria-label="Waving Hand">
+					👋🏾
+				</span>
+				<MailText>alukodotun@gmail.com</MailText>
+				Or perhaps a Call
+				<NumberText>0803.769.9140</NumberText>
+				<CopyrightText>
+					&copy; {months[new Date().getMonth()]}, {new Date().getFullYear()}{" "}
+					Oladotun Aluko
+				</CopyrightText>
+			</div>
+		</FooterWrapper>
+	);
+}
