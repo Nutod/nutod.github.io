bfea6bde29b7ff29e2e0b5b9131cac381aee586c
Nutod <alukodotun@gmail.com>
2019-01-31 10:36:08 +0100

    Added SideProjects Component to Layout

--- src/container/Layout.tsx
+++ src/container/Layout.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
 import Footer from "../components/Footer/Footer";
+import SideProject from "../components/SideProjects/SideProjects";
 
 export default function Layout() {
 	return (
 		<>
 			<Hero />
+			<SideProject />
 			<Footer />
 		</>
 	);
