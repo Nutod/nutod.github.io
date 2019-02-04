7571e8971d3bf9e1b8daed8c44f93b2b12e18f0a
Nutod <alukodotun@gmail.com>
2019-02-04 13:37:09 +0100

    Changed logic in SideProjects Component

--- src/components/SideProjects/SideProjects.tsx
+++ src/components/SideProjects/SideProjects.tsx
@@ -13,32 +13,33 @@ import {
 	CardLink
 } from "../styles/Card";
 
+const data = [
+	{
+		title: "Burger Builder",
+		from: "#F0CB35",
+		to: "#C02425",
+		url: "http://burger-react-d3b90.firebaseapp.com"
+	},
+	{
+		title: "Teams App",
+		from: "#F15F79",
+		to: "#B24592",
+		url: "https://codesandbox.io/s/m5v40q9n2p"
+	},
+	{
+		title: "Robots App",
+		from: "#DBD5A4",
+		to: "#649173",
+		url: "https://codesandbox.io/s/xojv3zw5mp"
+	}
+];
+
 export default function SideProjects() {
-	const state = [
-		{
-			title: "Burger Builder",
-			from: "#F0CB35",
-			to: "#C02425",
-			url: "http://burger-react-d3b90.firebaseapp.com"
-		},
-		{
-			title: "Teams App",
-			from: "#F15F79",
-			to: "#B24592",
-			url: "https://codesandbox.io/s/m5v40q9n2p"
-		},
-		{
-			title: "Robots App",
-			from: "#DBD5A4",
-			to: "#649173",
-			url: "https://codesandbox.io/s/xojv3zw5mp"
-		}
-	];
 	return (
 		<SideProjectWrapper>
 			<SectionHeader>Side Projects</SectionHeader>
 			<CardContainer>
-				{state.map(project => (
+				{data.map(project => (
 					<Card>
 						<CardSide>
 							<CardSideFront>
