ea8f77e01ea887467e7606bf8e2660076240adb8
Nutod <alukodotun@gmail.com>
2019-02-05 10:16:10 +0100

    Updates

---
+++ precache-manifest.2967d5429970a4d7dcadc8db0e9e19de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c32f0c6dcbf85f3ca981769c29f476b",
+    "url": "/static/media/kudi-min.1c32f0c6.png"
+  },
+  {
+    "revision": "a03d33a59f333be83cefa5c9d8b7c348",
+    "url": "/static/media/hero-bg.a03d33a5.png"
+  },
+  {
+    "revision": "5132f1791554f53e87482a189e95563b",
+    "url": "/static/media/cleaners-min.5132f179.png"
+  },
+  {
+    "revision": "11928719cf84128ede60d18df606b857",
+    "url": "/static/media/capro-min.11928719.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9ecc29c26b83bab3a5d",
+    "url": "/static/js/main.b9ecc29c.chunk.js"
+  },
+  {
+    "revision": "0c50dff8151264af1932",
+    "url": "/static/js/1.0c50dff8.chunk.js"
+  },
+  {
+    "revision": "b9ecc29c26b83bab3a5d",
+    "url": "/static/css/main.d24c2849.chunk.css"
+  },
+  {
+    "revision": "4fc2d071773b20a9ae8a6e0675d2ab0e",
+    "url": "/index.html"
+  }
+];
